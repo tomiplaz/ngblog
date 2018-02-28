df1c949016764670ed90d2164ae26a81b2bf238a
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-02-28 22:32:34 +0100

    Add Post interface. Define apiUrl in environment. Define some methods in PostsService.

--- src/app/core/api/posts.service.ts
+++ src/app/core/api/posts.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
+
+import { Post } from '../../posts/post.interface';
 
 @Injectable()
 export class PostsService {
 
-  constructor() { }
+  private static baseUrl = environment.apiUrl + '/posts';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts() {
+    return this.httpClient.get<Post[]>(PostsService.baseUrl);
+  }
+  
+  getPost(stringId: string) {
+    return this.httpClient.get<Post>(PostsService.baseUrl + '/' + stringId);
+  }
+
+  createPost(post: Post) {
+    return this.httpClient.post<Post>(PostsService.baseUrl, post);
+  }
 
 }
---
+++ src/app/posts/post.interface.ts
@@ -0,0 +1,7 @@
+export interface Post {
+    id?: number,
+    string_id?: string,
+    user_id: number,
+    title: string,
+    content: string
+}
