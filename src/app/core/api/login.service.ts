f1bc02c686a4d10fad174f05b6eaf343b1a605ee
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-09-20 19:30:38 +0200

    Drop unused code.

--- src/app/core/api/login.service.ts
+++ src/app/core/api/login.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/share';
