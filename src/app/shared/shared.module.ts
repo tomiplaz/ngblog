b0077a65c51854d697cd2c634a1ae379d1153583
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-03 18:14:40 +0200

    Drop unused post-item module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PostItemModule } from './post-item/post-item.module';
-
 @NgModule({
   imports: [
     CommonModule,
-    PostItemModule
   ],
   declarations: []
 })
