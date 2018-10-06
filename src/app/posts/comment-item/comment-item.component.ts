2b477172af311887020a48847bfc5a088a5b094c
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-06 18:13:39 +0200

    Add comment-item component.

---
+++ src/app/posts/comment-item/comment-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Comment } from '../comment.interface';
+
+@Component({
+  selector: 'app-comment-item',
+  templateUrl: './comment-item.component.html',
+  styleUrls: ['./comment-item.component.css']
+})
+export class CommentItemComponent implements OnInit {
+
+  @Input() comment: Comment;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (!this.comment) {
+      throw new Error('CommentItemComponent requires comment attribute!');
+    }
+  }
+
+}
