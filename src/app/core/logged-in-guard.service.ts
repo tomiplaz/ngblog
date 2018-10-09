14484b521b4c83b6c28b05f52db0c99a848b35c8
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-09 19:19:12 +0200

    Fix guard by using a new observable.

--- src/app/core/logged-in-guard.service.ts
+++ src/app/core/logged-in-guard.service.ts
@@ -10,7 +10,7 @@ export class LoggedInGuard implements CanActivate {
   ) { }
 
   canActivate() {
-    return this.loginService.isLoggedInObservable;
+    return Boolean(this.loginService.loggedInUserObservable);
   }
 
 }
