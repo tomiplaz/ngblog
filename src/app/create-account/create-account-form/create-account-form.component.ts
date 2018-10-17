9b8bd90f791a070ab18ce14d77eeff403865f2a6
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-17 12:11:22 +0200

    Update template and style of create-account and create-account-form components.

--- src/app/create-account/create-account-form/create-account-form.component.ts
+++ src/app/create-account/create-account-form/create-account-form.component.ts
@@ -8,7 +8,10 @@ import { MessageService } from '../../core/message.service';
 @Component({
   selector: 'app-create-account-form',
   templateUrl: './create-account-form.component.html',
-  styleUrls: ['./create-account-form.component.css']
+  styleUrls: [
+    '../../shared/shared.css',
+    './create-account-form.component.css',
+  ]
 })
 export class CreateAccountFormComponent implements OnInit {
 
--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.css']
+  styleUrls: [
+    '../shared/shared.css',
+    './create-account.component.css',
+  ]
 })
 export class CreateAccountComponent implements OnInit {
 
