32c7a7029b7136a3139275ebbbc43a8bcde7c1e0
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-17 11:57:40 +0200

    Update template and style of login and login-form components.

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -8,7 +8,10 @@ import { MessageService } from '../../core/message.service';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: [
+    '../../shared/shared.css',
+    './login-form.component.css',
+  ]
 })
 export class LoginFormComponent implements OnInit {
 
