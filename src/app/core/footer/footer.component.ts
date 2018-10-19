a3efa8e3c2b99660614be1892fc98780d10ba658
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-19 19:35:08 +0200

    Add footer component.

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SettingsService } from '../settings.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Theme } from '../settings.values';
+
+enum ThemeCharacter {
+  STAR = '&#x02606;',
+  FSTAR = '&#x02605;',
+}
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: [
+    '../../shared/shared.css',
+    './footer.component.css',
+  ]
+})
+export class FooterComponent implements OnInit, OnDestroy {
+
+  isLight: boolean;
+  isDark: boolean;
+  themeCharacter: ThemeCharacter;
+  private themeSubscription: Subscription;
+
+  constructor(private settingsService: SettingsService) { }
+
+  ngOnInit() {
+    this.themeSubscription = this.settingsService.theme$
+      .subscribe(theme => {
+        this.isLight = theme === Theme.Light;
+        this.isDark = theme === Theme.Dark;
+        this.themeCharacter = theme === Theme.Light ? ThemeCharacter.FSTAR : ThemeCharacter.STAR;
+      });
+  }
+
+  toggleTheme() {
+    this.settingsService.changeTheme(this.isLight ? Theme.Dark : Theme.Light);
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
+
+}
