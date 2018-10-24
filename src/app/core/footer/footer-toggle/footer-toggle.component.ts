72f6e79167a398c9ee8b8392fb0090e46fd6fd97
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-24 12:09:18 +0200

    Fix border color of header and footer toggles on theme change. Decrease padding.

--- src/app/core/footer/footer-toggle/footer-toggle.component.ts
+++ src/app/core/footer/footer-toggle/footer-toggle.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, HostListener, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, HostListener, OnDestroy, Output, EventEmitter, HostBinding } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs/Subscription';
+import { SettingsService, Theme } from '../../settings.service';
 
 @Component({
   selector: 'app-footer-toggle',
@@ -15,23 +16,31 @@ export class FooterToggleComponent implements OnInit, OnDestroy {
 
   @Output() toggled: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  @HostBinding('class.light') isLight: boolean;
+  @HostBinding('class.dark') isDark: boolean;
+
   readonly CLOSED_TEXT = 'Show';
   readonly OPENED_TEXT = 'Hide';
 
   private isClosed: boolean;
   private isClosedSubject: Subject<boolean> = new BehaviorSubject<boolean>(false);
   private isClosedSubscription: Subscription;
-  isClosed$ = this.isClosedSubject.asObservable();
+  private themeSubscription: Subscription;
 
+  isClosed$ = this.isClosedSubject.asObservable();
   toggleText: string;
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit() {
     this.isClosedSubscription = this.isClosed$.subscribe(isClosed => {
       this.isClosed = isClosed;
       this.toggleText = isClosed ? this.CLOSED_TEXT : this.OPENED_TEXT;
       this.toggled.emit(isClosed);
+    });
+    this.themeSubscription = this.settingsService.theme$.subscribe(theme => {
+      this.isLight = theme === Theme.Light;
+      this.isDark = theme === Theme.Dark;
     });
   }
 
@@ -42,6 +51,7 @@ export class FooterToggleComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.isClosedSubscription.unsubscribe();
+    this.themeSubscription.unsubscribe();
   }
 
 }
