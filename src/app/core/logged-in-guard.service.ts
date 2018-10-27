15e6f6bab7759f3421965ea47ab43e4116ba4f48
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-27 15:44:48 +0200

    Replace manual select with imported selector.

--- src/app/core/logged-in-guard.service.ts
+++ src/app/core/logged-in-guard.service.ts
@@ -3,6 +3,7 @@ import { CanActivate } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { AppState } from './store/store';
+import { selectIsLoggedIn } from './store/auth/auth.selectors';
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
@@ -10,7 +11,7 @@ export class LoggedInGuard implements CanActivate {
   constructor(private store: Store<AppState>) { }
 
   canActivate(): Observable<boolean> {
-    return this.store.pipe(select(state => state.auth.isLoggedIn));
+    return this.store.pipe(select(selectIsLoggedIn));
   }
 
 }
