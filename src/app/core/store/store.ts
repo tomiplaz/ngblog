7802a91bb143cbc262ea319e85917d4860e9c895
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-27 11:39:35 +0200

    Add store's feature selectors.

--- src/app/core/store/store.ts
+++ src/app/core/store/store.ts
@@ -1,6 +1,7 @@
 import { authReducer, AuthState } from "./auth/auth.reducer";
 import { settingsReducer, SettingsState } from "./settings/settings.reducer";
 import { sessionReducer, SessionState } from "./session/session.reducer";
+import { createFeatureSelector } from "@ngrx/store";
 
 export interface AppState {
   auth: AuthState,
@@ -13,3 +14,9 @@ export const store = {
   settings: settingsReducer,
   session: sessionReducer,
 };
+
+export const authSelector = createFeatureSelector<AuthState>('auth');
+
+export const settingsSelector = createFeatureSelector<SettingsState>('settings');
+
+export const sessionSelector = createFeatureSelector<SessionState>('session');
