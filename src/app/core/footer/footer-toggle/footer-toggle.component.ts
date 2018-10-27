319e2a6d302cad7aa03abdba66c5a53865644a77
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-27 12:19:11 +0200

    Refactor observing store values in footer toggle.

--- src/app/core/footer/footer-toggle/footer-toggle.component.ts
+++ src/app/core/footer/footer-toggle/footer-toggle.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, HostListener, HostBinding } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component, OnInit, HostListener, HostBinding, OnDestroy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { Theme } from '../../store/settings/settings.values';
 import { AppState } from '../../store/store';
 import { ToggleFooter } from '../../store/session/session.actions';
+import { Subscription } from 'rxjs/Subscription';
+import { selectTheme } from '../../store/settings/settings.selectors';
+import { selectIsFooterOpen } from '../../store/session/session.selectors';
 
 @Component({
   selector: 'app-footer-toggle',
@@ -12,27 +15,31 @@ import { ToggleFooter } from '../../store/session/session.actions';
     './footer-toggle.component.css',
   ]
 })
-export class FooterToggleComponent implements OnInit {
+export class FooterToggleComponent implements OnInit, OnDestroy {
 
   @HostBinding('class') classAttribute: Theme;
 
   readonly CLOSED_TEXT = 'Show';
   readonly OPEN_TEXT = 'Hide';
 
-  toggleText: string;
+  private themeSubscription: Subscription;
+  isFooterOpen$ = this.store.pipe(select(selectIsFooterOpen));
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.store.subscribe(store => {
-      this.classAttribute = store.settings.theme;
-      this.toggleText = store.session.isFooterOpen ? this.OPEN_TEXT : this.CLOSED_TEXT;
+    this.themeSubscription = this.store.pipe(select(selectTheme)).subscribe(theme => {
+      this.classAttribute = theme;
     });
   }
 
   @HostListener('click')
   onClick() {
     this.store.dispatch(new ToggleFooter());
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
   }
 
 }
