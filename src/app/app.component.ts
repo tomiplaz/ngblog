0bf19cf1597ead414ccd5d7adb9a281e38c976c7
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-28 17:15:51 +0100

    Open / close header based on scroll. Dispatch actions for disabling / enabling header toggle.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, HostBinding, HostListener, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from './core/store/store';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import { AppState } from './core/store/store';
+import { ToggleHeader, EnableHeaderToggle, DisableHeaderToggle } from './core/store/session/session.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +13,69 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-  @ViewChild('content') content: ElementRef;
+  readonly TOP_HORIZONTAL = 90;
+  readonly TOP_VERTICAL = 131;
+  readonly TOP_EXTRA = 50;
+
+  @ViewChild('content') contentElementRef: ElementRef;
 
   @HostBinding('class') classAttribute: string;
 
+  private isHeaderOpen: boolean;
+  private isLoggedIn: boolean;
+
+  private scroll$ = Observable.fromEvent(window, 'scroll');
+
   private storeSubscription: Subscription;
+  private scrollSubscription: Subscription;
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.storeSubscription = this.store.subscribe(store => {
+    this.storeSubscription = this.store.subscribe(state => {
       this.classAttribute = [
-        store.settings.theme,
-        store.settings.size,
-        ...store.session.isHeaderOpen ? [] : ['header-closed'],
-        ...store.auth.isLoggedIn ? [] : ['header-list-horizontal'],
+        state.settings.theme,
+        state.settings.size,
+        ...state.session.isHeaderOpen ? [] : ['header-closed'],
+        ...state.auth.isLoggedIn ? [] : ['header-list-horizontal'],
       ].join(' ');
+
+      this.isHeaderOpen = state.session.isHeaderOpen;
+      this.isLoggedIn = state.auth.isLoggedIn;
+    });
+
+    this.scrollSubscription = this.scroll$.subscribe(() => {
+      const top = this.contentElementRef.nativeElement.getBoundingClientRect().top;
+      const width = window.innerWidth;
+      const isHeaderListVertical = width <= 600 && this.isLoggedIn;
+
+      if (
+        (isHeaderListVertical && top > this.TOP_VERTICAL - this.TOP_EXTRA) ||
+        (!isHeaderListVertical && top > this.TOP_HORIZONTAL - this.TOP_EXTRA)
+      ) {
+        this.freezeOpenHeader();
+      } else {
+        this.store.dispatch(new EnableHeaderToggle());
+
+        if (
+          (isHeaderListVertical && top < this.TOP_VERTICAL - this.TOP_EXTRA) ||
+          (!isHeaderListVertical && top < this.TOP_HORIZONTAL - this.TOP_EXTRA)
+        ) {
+          if (this.isHeaderOpen) this.store.dispatch(new ToggleHeader());
+        }
+      }
     });
   }
 
-  @HostListener('window:scroll')
-  onScroll() {
-    const top = this.content.nativeElement.getBoundingClientRect().top;
+  private freezeOpenHeader() {
+    this.store.dispatch(new DisableHeaderToggle());
+
+    if (!this.isHeaderOpen) this.store.dispatch(new ToggleHeader());
   }
 
   ngOnDestroy() {
     this.storeSubscription.unsubscribe();
+    this.scrollSubscription.unsubscribe();
   }
 
 }
