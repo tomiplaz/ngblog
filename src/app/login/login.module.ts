c88ca76b5fd37e51f8c5c31e8421a2dd693007e9
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-11-21 18:10:32 +0100

    Use error-messages component for login form. Add labels as well.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { LoginRoutingModule } from './login-routing.module';
-
 import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    SharedModule
   ],
   declarations: [
     LoginComponent,
