6fcb89a396d59a1f44baf2f60837283f64a0cf68
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-06 15:41:19 +0100

    Update imports and export component to fix errors.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorMessagesComponent } from './error-messages/error-messages.component';
 import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [
     ErrorMessagesComponent,
+    ChangePasswordFormComponent,
   ],
   declarations: [
     ErrorMessagesComponent,
