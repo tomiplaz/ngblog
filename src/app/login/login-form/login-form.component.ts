60cd0b398fecc6467f005fa7a5209225b3da2ee2
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-07 13:35:45 +0100

    Fix setting validators, attributes' values.

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -25,7 +25,7 @@ export class LoginFormComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: [null, Validators.required, Validators.email],
+      email: [null, [Validators.required, Validators.email]],
       password: [null, Validators.required]
     });
   }
