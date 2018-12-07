d2be8c12685e86b28796978c81af04eadc42c5bc
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-07 13:16:00 +0100

    Add missing import.

--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -5,12 +5,14 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyProfileFormComponent } from './my-profile/my-profile-form/my-profile-form.component';
 import { ChangePasswordFormComponent } from './my-profile/change-password-form/change-password-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MyProfileRoutingModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   declarations: [
     MyProfileComponent,
