8dfeb61548cf8d9b1d17521a9c8452dc68ca3984
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-07 12:37:39 +0100

    Update URLs for auth endpoints. Fix parameter name.

--- src/app/core/api/auth.service.ts
+++ src/app/core/api/auth.service.ts
@@ -14,11 +14,12 @@ export interface Credentials {
 @Injectable()
 export class AuthService {
 
+  readonly BASE_URL = `${environment.apiUrl}/auth`;
+
   constructor(private httpClient: HttpClient, private store: Store<AppState>) { }
 
   login(credentials: Credentials): Observable<any> {
-    const url = `${environment.apiUrl}/login`;
-    const observable = this.httpClient.post<any>(url, credentials).share();
+    const observable = this.httpClient.post<any>(`${this.BASE_URL}/login`, credentials).share();
 
     observable.subscribe(response => {
       this.store.dispatch(new Login(response.token, response.user));
@@ -31,10 +32,8 @@ export class AuthService {
     this.store.dispatch(new Logout());
   }
 
-  forgotPassword(name: string): Observable<any> {
-    const url = `${environment.apiUrl}/forgot-password`;
-
-    return this.httpClient.post<any>(url, { name });
+  forgotPassword(email: string): Observable<any> {
+    return this.httpClient.post<any>(`${this.BASE_URL}/forgot-password`, { email });
   }
 
 }
