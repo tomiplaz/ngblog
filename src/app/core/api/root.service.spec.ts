5a49a2611e34eb93f10d44d2c037def447e8f9d2
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-11 12:14:55 +0100

    Add unit tests for RootService.

--- src/app/core/api/root.service.spec.ts
+++ src/app/core/api/root.service.spec.ts
@@ -1,15 +1,49 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Observable } from 'rxjs/Observable';
 import { RootService } from './root.service';
+import { ApiModule } from './api.module';
 
-describe('RootService', () => {
+fdescribe('RootService', () => {
+  let service: RootService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ApiModule, HttpClientTestingModule],
       providers: [RootService]
     });
+
+    service = TestBed.get(RootService);
   });
 
-  it('should be created', inject([RootService], (service: RootService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  describe('method', () => {
+    let httpTC: HttpTestingController;
+
+    beforeEach(() => {
+      httpTC = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+      httpTC.verify();
+    });
+
+    describe('#getHomeData', () => {
+      it('should return an Observable', () => {
+        const value = service.getHomeData();
+  
+        expect(value instanceof Observable).toBeTruthy();
+      });
+  
+      it('should send a request to get home data when subscribed', () => {
+        service.getHomeData().subscribe();
+  
+        const mockRequest = httpTC.expectOne(`${service.BASE_URL}/home`);
+        expect(mockRequest.request.method).toBe('GET');
+      });
+    });
+  });
 });
