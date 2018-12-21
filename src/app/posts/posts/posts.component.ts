278f43c6573547bb87e73de1ed11397cc8259000
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-21 15:03:21 +0100

    Emit with order component and use it in posts. Method defined to enable refactoring.

--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Post } from '../post.interface';
 import { CommonService } from '../../core/common.service';
 import { MessageService } from '../../core/message.service';
@@ -18,6 +18,7 @@ export class PostsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private commonService: CommonService,
     private messageService: MessageService,
     private postsService: PostsService,
@@ -43,6 +44,14 @@ export class PostsComponent implements OnInit {
     }, error => {
       this.messageService.error(error);
     })
+  }
+
+  onParamChanged(param: Params) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { ...param },
+      queryParamsHandling: 'merge',
+    });
   }
 
 }
--- src/app/shared/order/order.component.ts
+++ src/app/shared/order/order.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, OnDestroy {
 
   order = new FormControl('desc');
+  valueSubscription: Subscription;
+  @Output() changed = new EventEmitter<{ order: string }>();
 
   constructor() { }
 
   ngOnInit() {
-    this.order.valueChanges.subscribe(value => {
-      //
+    this.valueSubscription = this.order.valueChanges.subscribe(value => {
+      this.changed.emit({ order: value });
     });
+  }
+
+  ngOnDestroy() {
+    this.valueSubscription.unsubscribe();
   }
 
 }
