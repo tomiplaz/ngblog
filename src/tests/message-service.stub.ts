c6ee1f46911e36097de695dea6cc21dc9ea31885
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-03 20:36:45 +0100

    Update messageServiceStub.

--- src/tests/message-service.stub.ts
+++ src/tests/message-service.stub.ts
@@ -2,8 +2,15 @@ import { MessageService } from '../app/core/message.service';
 
 export const messageServiceStub: Partial<MessageService> = {
   createAccountSuccess: () => {},
+  confirmAccountSuccess: () => {},
   loginSuccess: () => {},
+  logoutSuccess: () => {},
   createPostSuccess: () => {},
   createCommentSuccess: () => {},
+  updateMyProfileSuccess: () => {},
+  changePasswordSuccess: () => {},
+  forgotPasswordEmailSent: () => {},
+  forgotPasswordEmailRequired: () => {},
+  resetPasswordSuccess: () => {},
   error: (response: any) => {},
 };
