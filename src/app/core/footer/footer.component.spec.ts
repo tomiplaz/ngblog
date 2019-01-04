113f7a520cac4313ce6f52ab21130f3bf1b102e7
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-04 20:25:24 +0100

    Cover FooterComponent with unit tests.

--- src/app/core/footer/footer.component.spec.ts
+++ src/app/core/footer/footer.component.spec.ts
@@ -5,8 +5,10 @@ import { FooterComponent } from './footer.component';
 import { sessionReducer } from '../store/session/session.reducer';
 import { settingsReducer } from '../store/settings/settings.reducer';
 import { AppState } from '../store/store';
+import { ToggleFooter } from '../store/session/session.actions';
+import { ToggleSize, ToggleTheme } from '../store/settings/settings.actions';
 
-fdescribe('FooterComponent', () => {
+describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
   let store: Store<AppState>;
@@ -29,16 +31,71 @@ fdescribe('FooterComponent', () => {
     store = TestBed.get(Store);
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
-  xit('should subscribe to store on init', () => {
-    const storeSubscribeSpy = spyOn(store, 'subscribe');
+  it('should subscribe to store on init', () => {
+    const storeSubscribeSpy = spyOn(store, 'subscribe').and.callThrough();
+
+    fixture.detectChanges();
 
     expect(storeSubscribeSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should have isFooterOpen prop initialized to store value', () => {
+    fixture.detectChanges();
+
+    expect(component.isFooterOpen).toBe(false);
+  });
+
+  it('should have classAttribute prop initialized based on store values', () => {
+    fixture.detectChanges();
+
+    expect(component.classAttribute).toBe('dark medium closed');
+  });
+
+  it('should have classAttribute prop initialized with no closed class if footer is open', () => {
+    store.dispatch(new ToggleFooter());
+
+    fixture.detectChanges();
+
+    expect(component.classAttribute).toBe('dark medium');
+  });
+
+  it('#onToggleClick should dispatch ToggleFooter action', () => {
+    const storeDispatchSpy = spyOn(store, 'dispatch');
+
+    fixture.detectChanges();
+
+    component.onToggleClick();
+
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+    expect(storeDispatchSpy).toHaveBeenCalledWith(new ToggleFooter());
+  });
+
+  it('#onSizeClick should dispatch ToggleSize action', () => {
+    const storeDispatchSpy = spyOn(store, 'dispatch');
+
+    fixture.detectChanges();
+
+    component.onSizeClick();
+
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+    expect(storeDispatchSpy).toHaveBeenCalledWith(new ToggleSize());
+  });
+
+  it('#onThemeClick should dispatch ToggleTheme action', () => {
+    const storeDispatchSpy = spyOn(store, 'dispatch');
+
+    fixture.detectChanges();
+
+    component.onThemeClick();
+
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+    expect(storeDispatchSpy).toHaveBeenCalledWith(new ToggleTheme());
   });
 });
