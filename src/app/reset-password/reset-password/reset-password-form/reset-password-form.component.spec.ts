462205066a4eaa4eb8b1bf526520acd618f818df
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-06 19:17:15 +0100

    Make reset-password-form component spec green.

--- src/app/reset-password/reset-password/reset-password-form/reset-password-form.component.spec.ts
+++ src/app/reset-password/reset-password/reset-password-form/reset-password-form.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ResetPasswordFormComponent } from './reset-password-form.component';
+import { CommonService } from 'app/core/common.service';
+import { AuthService } from 'app/core/api/auth.service';
+import { authServiceStub } from 'tests/auth-service.stub';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'app/core/message.service';
+import { messageServiceStub } from 'tests/message-service.stub';
 
 describe('ResetPasswordFormComponent', () => {
   let component: ResetPasswordFormComponent;
@@ -8,7 +15,16 @@ describe('ResetPasswordFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResetPasswordFormComponent ]
+      imports: [ ReactiveFormsModule ],
+      declarations: [ ResetPasswordFormComponent ],
+      providers: [
+        CommonService,
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: ActivatedRoute, useValue: {} },
+        { provide: MessageService, useValue: messageServiceStub },
+        { provide: Router, useValue: {} },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
