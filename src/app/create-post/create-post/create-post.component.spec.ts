969e87053c86b5325d396514e752edcc248bdc79
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-06 18:32:35 +0100

    Make spec for create-post component green.

--- src/app/create-post/create-post/create-post.component.spec.ts
+++ src/app/create-post/create-post/create-post.component.spec.ts
@@ -1,5 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreatePostComponent } from './create-post.component';
+import { PostsService } from 'app/core/api/posts.service';
+import { MessageService } from 'app/core/message.service';
+import { messageServiceStub } from 'tests/message-service.stub';
 
 describe('CreatePostComponent', () => {
   let component: CreatePostComponent;
@@ -8,6 +13,12 @@ describe('CreatePostComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreatePostComponent ],
+      providers: [
+        { provide: Router, useValue: {} },
+        { provide: PostsService, useValue: {} },
+        { provide: MessageService, useValue: messageServiceStub },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
