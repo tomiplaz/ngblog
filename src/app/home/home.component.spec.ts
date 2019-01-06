f8215505ce75f0fb0f44ea8a96d0b97f5439239f
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-06 18:56:26 +0100

    Make home component spec green.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 import { HomeComponent } from './home.component';
+import { CommonService } from 'app/core/common.service';
+import { HomeData } from './home-data.interface';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
+    const data: HomeData = {
+      posts: {
+        recent: [],
+        random: [],
+      },
+      stats: {
+        users_count: null,
+        posts_count: null,
+        comments_count: null,
+      },
+    };
+    const routeStub = { data: of({ data }) };
+
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: routeStub },
+        { provide: CommonService, useValue: {} },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
