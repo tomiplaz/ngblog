a6f5e664f216030eb48fc83902585e25b7299af0
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-06 19:52:18 +0100

    Make breadcrumbs component spec green.

--- src/app/core/header/breadcrumbs/breadcrumbs.component.spec.ts
+++ src/app/core/header/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 
 describe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
 
+  const routerStub = {
+    navigate: () => {},
+    events: new Subject<Event>(),
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BreadcrumbsComponent ]
+      declarations: [ BreadcrumbsComponent ],
+      providers: [
+        { provide: Router, useValue: routerStub },
+      ],
     })
     .compileComponents();
   }));
