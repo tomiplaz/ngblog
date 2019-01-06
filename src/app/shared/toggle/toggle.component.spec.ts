e4857b7ab62b9aa79e3c9262f3c4616d2069cd8b
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-06 19:55:01 +0100

    Make toggle component spec green.

--- src/app/shared/toggle/toggle.component.spec.ts
+++ src/app/shared/toggle/toggle.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ToggleComponent } from './toggle.component';
 
 describe('ToggleComponent', () => {
@@ -8,7 +8,8 @@ describe('ToggleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToggleComponent ]
+      declarations: [ ToggleComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
