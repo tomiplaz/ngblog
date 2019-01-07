d205d88fb1c05ee361490abd9cec58ab8ae9fa90
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-07 13:38:28 +0100

    Fix e2e expected value for title.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ngblog App', () => {
+describe('blogs app', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('ngblog App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('blogs');
   });
 });
