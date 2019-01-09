0bfa58d439e9756f2a691b721e705937b7b90b5a
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-01-09 12:58:48 +0100

    Refactor e2e spec for app page.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('blogs app', () => {
+describe('app page', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('blogs');
+    expect(page.getTitle()).toEqual('blogs');
   });
 });
